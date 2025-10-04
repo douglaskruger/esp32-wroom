e97558a22442ce74c91de34691a806fbefda911f
Douglas Kruger <douglas.d.kruger@gmail.com>
2025-10-05 11:00:40 +1300

    Create esp32_temp_humidity_web.c

---
+++ esp32_temp_humidity_web.c
@@ -0,0 +1,190 @@
+/* =============================================================================
+   Copyright © 2025 Skynet Consulting Ltd.
+
+   This file is the property of Skynet Consulting Ltd. and shall not be reproduced,
+   copied, or used as the basis for the manufacture or sale of equipment without
+   the expressed written permission of Skynet Consulting Ltd.
+   =============================================================================
+*/
+static const char module_id[] __attribute__((used)) = "$Id: set_gpio.c $";
+
+/*
+ * Description:
+ * The following program presents a web based temperature and humidity sensor 
+ * on the ESP32C - WROOM
+*/
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <DHT.h>
+#include "time.h"
+
+const char *ssid = "Skynet-Starlink";
+const char *password = "HillTop!";
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 12*3600;
+const int   daylightOffset_sec = 3600;
+char timeString[40];
+
+WebServer server(80);
+DHT dht(26, DHT11);
+
+void handleRoot() {
+  char msg[1500];
+
+  snprintf(msg, 1500,
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='4'/>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>\
+    <title>ESP32 DHT Server</title>\
+    <style>\
+    html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}\
+    h2 { font-size: 3.0rem; }\
+    p { font-size: 3.0rem; }\
+    .units { font-size: 1.2rem; }\
+    .dht-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}\
+    </style>\
+  </head>\
+  <body>\
+      <h2>ESP32 DHT Server!</h2>\
+      <p>\
+        <i class='fas fa-time' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>Time</span>\
+        <span>%s</span>\
+      </p>\
+      <p>\
+        <i class='fas fa-time' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>IP Address:</span>\
+        <span>%s</span>\
+      </p>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>Temperature</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-tint' style='color:#00add6;'></i>\
+        <span class='dht-labels'>Humidity</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&percnt;</sup>\
+      </p>\
+  </body>\
+</html>",
+           printLocalTime(),
+           WiFi.localIP(),
+           readDHTTemperature(), 
+           readDHTHumidity()
+          );
+  server.send(200, "text/html", msg);
+}
+
+void setup(void) {
+
+  Serial.begin(115200);
+  dht.begin();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+}
+
+void loop(void) {
+  server.handleClient();
+  printLocalTime();
+  delay(1);//allow the cpu to switch to other tasks
+}
+
+char * printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return "Error";
+  }
+  //strftime(timeString, 40, "%B %d %Y %H:%M:%S", &timeinfo);
+  strftime(timeString, 40, "%B %d %Y %H:%M:%S", &timeinfo);
+  return timeString;
+  //return "23:32";
+  /*
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  */
+}
+
+float readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.println(h);
+    return h;
+  }
+}
